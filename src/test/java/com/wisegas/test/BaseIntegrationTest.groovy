09b6675779fbdd34deb8b83a5f27503dd7482f79
Sage-Pierce <SagePierce@utexas.edu>
2016-03-07 12:36:58 -0700

    Cleaning library declarations and imports

--- src/test/java/com/wisegas/test/BaseIntegrationTest.groovy
+++ src/test/java/com/wisegas/test/BaseIntegrationTest.groovy
@@ -2,15 +2,10 @@ package com.wisegas.test
 
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.transaction.BeforeTransaction
-import org.springframework.test.context.transaction.TransactionConfiguration
-import org.springframework.transaction.PlatformTransactionManager
 import spock.lang.Shared
 import spock.lang.Specification
 
 import javax.inject.Inject
-import javax.persistence.EntityManager
-import javax.persistence.EntityManagerFactory
-import javax.persistence.PersistenceContext
 
 @ContextConfiguration("classpath*:META-INF/Beans.xml")
 abstract class BaseIntegrationTest extends Specification {
