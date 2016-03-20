90693ebf2ddc671dc379f4c6eecfbd4a1a151cd2
Sage-Pierce <SagePierce@utexas.edu>
2016-03-19 22:37:48 -0600

    More fucking of shit up

--- src/main/webapp/app/GrappConfigInit.js
+++ src/main/webapp/app/GrappConfigInit.js
@@ -5,7 +5,7 @@
 
    angular.element(document.getElementsByTagName('head')).append(angular.element('<base href="' + window.location.pathname + '" />'));
 
-   angular.module("Grapp", ["ngCookies", "ui.router", "angular-hal", "ui.bootstrap", "uiGmapgoogle-maps"])
+   angular.module("Grapp", ["ngCookies", "ui.router", "angular-hal", "ui.bootstrap", "uiGmapgoogle-maps", "ui.tree"])
       .config(grappConfig)
       .run(grappInit);
 
