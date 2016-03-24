1a9992ef58acf7debc05d2ef949f071710c37e19
Sage-Pierce <SagePierce@utexas.edu>
2016-03-23 21:02:24 -0600

    Fix verbosity

--- src/main/java/com/wisegas/common/webserver/jaxrs/hal/JaxrsHalJsonResource.java
+++ src/main/java/com/wisegas/common/webserver/jaxrs/hal/JaxrsHalJsonResource.java
@@ -5,7 +5,7 @@ import com.wisegas.common.webserver.hal.impl.HalJsonRepresentationFactory;
 
 import javax.ws.rs.Produces;
 
-@Produces({HalJsonRepresentationFactory.HAL_JSON})
+@Produces(HalJsonRepresentationFactory.HAL_JSON)
 public abstract class JaxrsHalJsonResource extends JaxrsHalResource {
    protected static final HalRepresentationFactory halRepresentationFactory = new HalJsonRepresentationFactory();
 }
