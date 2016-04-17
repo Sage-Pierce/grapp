a909140dfbd17eaa2917f4a106e06c10a21d02a2
Sage-Pierce <SagePierce@utexas.edu>
2016-04-17 07:43:39 -0600

    Removing HelloResource Security

--- src/main/java/com/wisegas/common/security/spring/jaxrs/provider/AuthenticationContextResolver.java
+++ src/main/java/com/wisegas/common/security/spring/jaxrs/provider/AuthenticationContextResolver.java
@@ -1,19 +1,19 @@
 package com.wisegas.common.security.spring.jaxrs.provider;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
-import java.security.Principal;
 
 @Provider
 @Produces(MediaType.WILDCARD)
-public class AuthenticationContextResolver implements ContextResolver<Principal> {
+public class AuthenticationContextResolver implements ContextResolver<Authentication> {
 
    @Override
-   public Principal getContext(Class<?> aClass) {
+   public Authentication getContext(Class<?> aClass) {
       return SecurityContextHolder.getContext().getAuthentication();
    }
 }
