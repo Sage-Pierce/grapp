71b617413044e2278fb9455d811ce0518a588bf2
Sage-Pierce <SagePierce@utexas.edu>
2016-05-06 08:49:23 -0600

    NodeDto and ShoppingNodeDto share some of the same traits; factor up to common base class

--- src/main/java/com/wisegas/storemanagement/service/dto/NodeDto.java
+++ src/main/java/com/wisegas/storemanagement/service/dto/NodeDto.java
@@ -1,31 +1,10 @@
 package com.wisegas.storemanagement.service.dto;
 
-import com.wisegas.common.lang.dto.NamedDto;
-import com.wisegas.common.lang.spacial.GeoPoint;
-
 import java.util.List;
 
-public class NodeDto extends NamedDto {
+public class NodeDto extends AbstractNodeDto {
 
-   private String type;
-   private GeoPoint location;
    private List<NodeItemDto> items;
-
-   public String getType() {
-      return type;
-   }
-
-   public void setType(String type) {
-      this.type = type;
-   }
-
-   public GeoPoint getLocation() {
-      return location;
-   }
-
-   public void setLocation(GeoPoint location) {
-      this.location = location;
-   }
 
    public List<NodeItemDto> getItems() {
       return items;
--- src/main/java/com/wisegas/storemanagement/service/dto/ShoppingNodeDto.java
+++ src/main/java/com/wisegas/storemanagement/service/dto/ShoppingNodeDto.java
@@ -1,31 +1,10 @@
 package com.wisegas.storemanagement.service.dto;
 
-import com.wisegas.common.lang.dto.NamedDto;
-import com.wisegas.common.lang.spacial.GeoPoint;
-
 import java.util.List;
 
-public class ShoppingNodeDto extends NamedDto {
+public class ShoppingNodeDto extends AbstractNodeDto {
 
-   private String type;
-   private GeoPoint location;
    private List<ShoppingItemDto> items;
-
-   public String getType() {
-      return type;
-   }
-
-   public void setType(String type) {
-      this.type = type;
-   }
-
-   public GeoPoint getLocation() {
-      return location;
-   }
-
-   public void setLocation(GeoPoint location) {
-      this.location = location;
-   }
 
    public List<ShoppingItemDto> getItems() {
       return items;
