628ff26b911258eb46fbc341d411c9c550d74946
Sage-Pierce <SagePierce@utexas.edu>
2016-05-08 08:50:33 -0500

    Abstract DTO is abstract

--- src/main/java/com/wisegas/storemanagement/service/dto/AbstractNodeDto.java
+++ src/main/java/com/wisegas/storemanagement/service/dto/AbstractNodeDto.java
@@ -3,7 +3,7 @@ package com.wisegas.storemanagement.service.dto;
 import com.wisegas.common.lang.dto.NamedDto;
 import com.wisegas.common.lang.spacial.GeoPoint;
 
-public class AbstractNodeDto extends NamedDto {
+public abstract class AbstractNodeDto extends NamedDto {
 
    private String type;
    private GeoPoint location;
