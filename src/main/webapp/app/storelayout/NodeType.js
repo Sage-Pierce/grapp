382d2f9859f34247955496f4d60d49f0511d2580
Sage-Pierce <SagePierce@utexas.edu>
2016-05-16 12:08:09 -0600

    Need 'code' to use as key in ShoppingPlan

--- src/main/webapp/app/storelayout/NodeType.js
+++ src/main/webapp/app/storelayout/NodeType.js
@@ -6,13 +6,14 @@
 
    function createEnum() {
       return {
-         REGULAR: new NodeType("Regular", "content/img/marker_blue.png"),
-         ENTRANCE: new NodeType("Entrance", "content/img/marker_green.png"),
-         EXIT: new NodeType("Exit", "content/img/marker_red.png")
+         REGULAR: new NodeType("R", "Regular", "content/img/marker_blue.png"),
+         ENTRANCE: new NodeType("E", "Entrance", "content/img/marker_green.png"),
+         EXIT: new NodeType("X", "Exit", "content/img/marker_red.png")
       };
    }
 
-   function NodeType(displayString, iconUrl) {
+   function NodeType(code, displayString, iconUrl) {
+      this.code = code;
       this.displayString = displayString;
       this.iconUrl = iconUrl;
    }
