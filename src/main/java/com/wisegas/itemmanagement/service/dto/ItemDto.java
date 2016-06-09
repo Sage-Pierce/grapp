e176db4ac1776b18edd9f965dedbb0ea72f41a46
Sage-Pierce <SagePierce@utexas.edu>
2016-06-09 07:58:53 -0600

    An ItemDto is an extended ItemLineageDto

--- src/main/java/com/wisegas/itemmanagement/service/dto/ItemDto.java
+++ src/main/java/com/wisegas/itemmanagement/service/dto/ItemDto.java
@@ -1,32 +1,11 @@
 package com.wisegas.itemmanagement.service.dto;
 
-import com.wisegas.common.lang.value.CodeName;
-
 import java.util.List;
 
-public class ItemDto {
+public class ItemDto extends ItemLineageDto {
 
-   private String primaryCode;
-   private String name;
    private String superItemCode;
-   private List<CodeName> lineage;
    private List<ItemDto> subItems;
-
-   public String getPrimaryCode() {
-      return primaryCode;
-   }
-
-   public void setPrimaryCode(String primaryCode) {
-      this.primaryCode = primaryCode;
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public void setName(String name) {
-      this.name = name;
-   }
 
    public String getSuperItemCode() {
       return superItemCode;
@@ -34,14 +13,6 @@ public class ItemDto {
 
    public void setSuperItemCode(String superItemCode) {
       this.superItemCode = superItemCode;
-   }
-
-   public List<CodeName> getLineage() {
-      return lineage;
-   }
-
-   public void setLineage(List<CodeName> lineage) {
-      this.lineage = lineage;
    }
 
    public List<ItemDto> getSubItems() {
