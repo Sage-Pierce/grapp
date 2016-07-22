bccfbe58de48d070d9fa667f27b3712a777be73e
Sage-Pierce <SagePierce@utexas.edu>
2016-07-22 11:20:00 -0600

    Restructuring bean configurations

--- src/main/java/com/wisegas/common/persistence/jpa/impl/GenericRepositoryImpl.java
+++ src/main/java/com/wisegas/common/persistence/jpa/impl/GenericRepositoryImpl.java
@@ -11,6 +11,7 @@ import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 public abstract class GenericRepositoryImpl<T extends SimpleEntity> implements GenericRepository<T> {
+
    @PersistenceContext
    protected EntityManager entityManager;
 
