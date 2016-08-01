4761075219b649699ef908997632a28ab72bddf5
Sage-Pierce <SagePierce@utexas.edu>
2016-07-31 20:28:05 -0500

    Rename

--- src/test/java/com/wisegas/common/test/util/FileToStringConverter.java
+++ src/test/java/com/wisegas/common/test/util/FileToStringConverter.java
@@ -6,16 +6,16 @@ import java.io.FileReader;
 
 public final class FileToStringConverter {
 
-   public static String convert(File xmlFile) {
+   public static String convert(File file) {
       StringBuilder stringBuilder = new StringBuilder();
-      try (BufferedReader bufferedReader = new BufferedReader(new FileReader(xmlFile))) {
+      try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
          String line;
          while ((line = bufferedReader.readLine()) != null) {
             stringBuilder.append(line);
          }
       }
       catch (Exception e) {
-         System.err.println("Exception reading File: " + xmlFile.getAbsolutePath());
+         System.err.println("Exception reading File: " + file.getAbsolutePath());
          e.printStackTrace();
       }
       return stringBuilder.toString();
