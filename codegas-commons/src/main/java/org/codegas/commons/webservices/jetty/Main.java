be1e8a6bd9ad3010524d681d14241dca34f7f398
spierce <spierce@homeaway.com>
2018-03-12 18:22:40 -0500

    [CODEGAS] [PATCH] Use better CLI parsing

--- codegas-commons/src/main/java/org/codegas/commons/webservices/jetty/Main.java
+++ codegas-commons/src/main/java/org/codegas/commons/webservices/jetty/Main.java
@@ -1,7 +1,9 @@
 package org.codegas.commons.webservices.jetty;
 
-import java.io.File;
-
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -11,21 +13,30 @@ import org.eclipse.jetty.webapp.WebAppContext;
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        String webAppDirectory = args.length == 0 ? "." : args[0];
+    private static final String WEB_APP_DIRECTORY_OPT = "wad";
 
-        // Not running WAR and 'root' is the context
-        WebAppContext contextHandler = new WebAppContext(null, "/");
+    private static final String PORT_OPT = "p";
+
+    private static final Options OPTIONS = new Options()
+        .addOption(Option.builder(WEB_APP_DIRECTORY_OPT).argName("webAppDirectory").hasArg()
+            .desc("Base directory of web app (containing WEB-INF directory)").build())
+        .addOption(Option.builder(PORT_OPT).argName("port").hasArg()
+            .desc("Port on which to run Application").build());
+
+    public static void main(String[] args) throws Exception {
+        CommandLine commandLine = new DefaultParser().parse(OPTIONS, args);
+
+        // Point at web app directory as the root context
+        WebAppContext contextHandler = new WebAppContext(commandLine.getOptionValue(WEB_APP_DIRECTORY_OPT, "src/main/webapp"), "/");
         // Parent loader priority is a class loader setting that Jetty accepts. By default Jetty will
         // behave like most web containers in that it will allow your application to replace
         // non-server libraries that are part of the container. Setting parent loader priority to
         // true changes this behavior. Read more: wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
         contextHandler.setParentLoaderPriority(true);
-        contextHandler.setResourceBase(new File(webAppDirectory, "src/main/webapp").getPath());
-        contextHandler.setDescriptor(contextHandler.getResourceBase() + "/WEB-INF/web.xml");
+        contextHandler.setDescriptor("WEB-INF/web.xml");
 
-        // Port to run on should be an environment variable. Default to 8080 if not set.
-        String port = System.getenv().getOrDefault("PORT", "8080");
+        // Port to run on should be an argument or environment variable. Default to 8080 if not set.
+        String port = commandLine.getOptionValue(PORT_OPT, System.getenv().getOrDefault("PORT", "8080"));
 
         Server server = new Server(Integer.valueOf(port));
         server.setHandler(contextHandler);
