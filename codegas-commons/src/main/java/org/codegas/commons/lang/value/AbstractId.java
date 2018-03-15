3c6e900762bbc47f807bbfa1f61e95e053291339
spierce <spierce@homeaway.com>
2018-03-15 08:58:26 -0500

    [SPIKE] [PATCH] Cleanup ID Hashing

--- codegas-commons/src/main/java/org/codegas/commons/lang/value/AbstractId.java
+++ codegas-commons/src/main/java/org/codegas/commons/lang/value/AbstractId.java
@@ -1,22 +1,25 @@
 package org.codegas.commons.lang.value;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public abstract class AbstractId implements Id {
 
     @Override
     public boolean equals(Object object) {
-        return object != null && getClass().equals(object.getClass()) && hashCode() == object.hashCode();
+        return object != null &&
+            getClass().equals(object.getClass()) &&
+            Objects.equals(idHash(), Id.class.cast(object).idHash());
     }
 
     @Override
     public int hashCode() {
-        return idHash().hashCode();
+        return Objects.hashCode(idHash());
     }
 
     @Override
     public String toString() {
-        return idHash().toString();
+        return Objects.toString(idHash());
     }
 
     protected static String generateValue() {
