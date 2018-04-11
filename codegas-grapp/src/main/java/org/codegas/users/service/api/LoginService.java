22befd94f48b1d0d37f070f5e3142be8babd0ccf
spierce <spierce@homeaway.com>
2018-04-10 19:57:52 -0500

    [SECURITY] [MINOR] Remove now-unused User and OAuth resources

--- codegas-grapp/src/main/java/org/codegas/users/service/api/LoginService.java
+++
@@ -1,9 +0,0 @@
-package org.codegas.users.service.api;
-
-import org.codegas.commons.lang.value.Email;
-import org.codegas.users.service.dto.UserDto;
-
-public interface LoginService {
-
-    UserDto logIn(Email email, String avatar);
-}
