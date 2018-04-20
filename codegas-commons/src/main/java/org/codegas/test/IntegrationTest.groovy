dbcddb8fe55c59968c6a1b3b7f4166f7d7c59b76
spierce <spierce@homeaway.com>
2018-04-20 17:20:33 -0500

    [SPIKE] [PATCH] Somehow missed some classes

--- codegas-commons/src/main/java/org/codegas/test/IntegrationTest.groovy
+++
@@ -1,8 +0,0 @@
-package org.codegas.test
-
-import org.springframework.test.context.ContextConfiguration
-
-@ContextConfiguration(["classpath*:META-INF/spring/test-context.xml"])
-abstract class IntegrationTest extends PersistenceTest {
-
-}
