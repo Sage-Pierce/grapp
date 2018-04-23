ed754e4c50fb111ce451692e64049c1d0e0dcd7d
spierce <spierce@homeaway.com>
2018-04-23 12:22:54 -0500

    [SPIKE] [PATCH] Source webservice and test deps from new projects

--- codegas-test/codegas-test-spock/src/main/java/org/codegas/test/spock/IntegrationTest.groovy
+++
@@ -1,8 +0,0 @@
-package org.codegas.test.spock
-
-import org.springframework.test.context.ContextConfiguration
-
-@ContextConfiguration(["classpath*:META-INF/spring/test-context.xml"])
-abstract class IntegrationTest extends PersistenceTest {
-
-}
--- codegas-webservice/codegas-webservice-hal/src/main/java/org/codegas/webservice/hal/api/HalRepresentationFactory.java
+++
@@ -1,8 +0,0 @@
-package org.codegas.webservice.hal.api;
-
-public interface HalRepresentationFactory {
-
-    HalRepresentation createFor(Object resource);
-
-    HalRepresentation createForLinks(Iterable<HalLink> links);
-}
